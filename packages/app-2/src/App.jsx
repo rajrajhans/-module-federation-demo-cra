2f2992c1e0264cd76152ca7090b715047104a727
RajRajhans <me@rajrajhans.com>
2021-10-24 15:45:04 +0530

    add cra and app-2 modules

---
+++ packages/app-2/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hello from app_2</div>;
+};
+
+export default App;
---
+++ packages/app-2/webpack.config.js
@@ -0,0 +1,43 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+const mode = process.env.NODE_ENV || "production";
+
+module.exports = {
+  mode,
+  entry: "./index.js",
+  output: {
+    publicPath: "http://localhost:3002/",
+  },
+  devtool: "source-map",
+  optimization: {
+    minimize: mode === "production",
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [require.resolve("@babel/preset-react")],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app_2",
+      library: {
+        type: "var",
+        name: "app_2",
+      },
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AppTwoComponent": "./src/App",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+};
